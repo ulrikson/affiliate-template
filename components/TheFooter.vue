a04013bc26ce0b15cf9a84170d4c95579504a193
ulrikson <erik.ulrikson@gmail.com>
2021-07-13 14:54:48 +0200

    footer updates

--- components/TheFooter.vue
+++ components/TheFooter.vue
@@ -1,6 +1,11 @@
 <template>
     <div class="bg-blue-nav bottom-0 mt-8 p-8 flex text-white">
-        <div class="w-1/2">About Affiliate</div>
-        <div class="w-1/2">Popular posts</div>
+        <div class="w-1/2 mr-4">
+            <h2>About Affiliate</h2>
+            <p class="mb-4">Animi asperiores ducimus in et ullam facilis nobis quibusdam. Nobis labore odio aut officiis adipisci. Similique laboriosam laboriosam qui. Eum quo libero voluptatem sed consequatur eius perferendis. Sequi qui asperiores aliquam ut.</p>
+        </div>
+        <div class="w-1/2">
+            <h2>Popular posts</h2>
+        </div>
     </div>
 </template>
