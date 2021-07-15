cec0cf43ee72420dd32dd28580b5a95658f13818
ulrikson <erik.ulrikson@gmail.com>
2021-07-15 13:12:06 +0200

    footer responsive

--- components/TheFooter.vue
+++ components/TheFooter.vue
@@ -1,10 +1,10 @@
 <template>
-    <div class="bg-blue-nav bottom-0 mt-8 p-8 flex text-white">
-        <div class="w-1/2 mr-4">
+    <div class="bg-blue-nav bottom-0 mt-8 p-8 md:flex text-white">
+        <div class="md:w-1/2 mr-4">
             <h2>About Affiliate</h2>
-            <p class="mb-4">Animi asperiores ducimus in et ullam facilis nobis quibusdam. Nobis labore odio aut officiis adipisci. Similique laboriosam laboriosam qui. Eum quo libero voluptatem sed consequatur eius perferendis. Sequi qui asperiores aliquam ut.</p>
+            <p class="mb-4">Animi asperiores ducimus in et ullam facilis nobis quibusdam. Nobis labore odio aut officiis adipisci.</p>
         </div>
-        <div class="w-1/2">
+        <div class="md:w-1/2">
             <h2>Popular posts</h2>
         </div>
     </div>
