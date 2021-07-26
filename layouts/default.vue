5fd1599f7e7b2725f927db0f021900d4aeb5bd41
ulrikson <erik.ulrikson@gmail.com>
2021-07-26 15:43:48 +0200

    styling changes

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,7 +1,7 @@
 <template>
     <div class="h-full font-sans">
         <the-navbar />
-        <Nuxt class="md:w-1/2 mx-auto"/>
+        <Nuxt class="px-16" />
         <the-footer />
     </div>
 </template>
--- pages/index.vue
+++ pages/index.vue
@@ -1,11 +1,6 @@
 <template>
     <div class="mx-auto px-4 my-4">
-        <h1 class="text-center mb-4">Affiliate</h1>
-        <img src="~/assets/landing.png" class="w-3/4 md:w-1/2 mx-auto"/>
-        <p class="mb-4">Animi asperiores ducimus in et ullam facilis nobis quibusdam. Nobis labore odio aut officiis adipisci. Similique laboriosam laboriosam qui. Eum quo libero voluptatem sed consequatur eius perferendis. Sequi qui asperiores aliquam ut.</p>
-        <div class="flex justify-center my-2">
-            <nuxt-link to="/" class="cta">Call to Action</nuxt-link>
-        </div>
+        <h1 class="mb-4">Affiliate</h1>
         <p class="mb-4">Animi asperiores ducimus in et ullam facilis nobis quibusdam. Nobis labore odio aut officiis adipisci. Similique laboriosam laboriosam qui. Eum quo libero voluptatem sed consequatur eius perferendis. Sequi qui asperiores aliquam ut.</p>
     </div>
 </template>
