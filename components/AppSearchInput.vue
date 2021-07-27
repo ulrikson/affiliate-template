74b27c2175e69f18028ef4ae26cacd7d31896312
ulrikson <erik.ulrikson@gmail.com>
2021-07-27 15:15:51 +0200

    search

--- components/AppSearchInput.vue
+++ components/AppSearchInput.vue
@@ -1,42 +1,36 @@
 <template>
-    <div>
-        <input
-            v-model="searchQuery"
-            type="search"
-            autocomplete="off"
-            placeholder="Search"
-        />
-        <ul v-if="articles.length">
-            <li v-for="article of articles" :key="article.slug">
-                <NuxtLink
-                    :to="{ name: 'blog-slug', params: { slug: article.slug } }"
-                >
-                    {{ article.title }}
-                </NuxtLink>
-            </li>
-        </ul>
-    </div>
+	<div class="w-full border-2 border-gray-bg border-solid rounded-md p-1">
+		<input v-model="searchQuery" type="search" autocomplete="off" placeholder="Search" class="w-full" />
+		<ul v-if="articles.length" class="absolute left-0 md:left-auto p-4 mx-4 md:mx-0 shadow-md rounded-md bg-white">
+			<li v-for="article of articles" :key="article.slug">
+				<NuxtLink :to="{ name: 'blog-slug', params: { slug: article.slug } }">
+					{{ article.title }}
+				</NuxtLink>
+			</li>
+		</ul>
+	</div>
 </template>
 
 <script>
 export default {
-    data() {
-        return {
-            searchQuery: "",
-            articles: []
-        };
-    },
-    watch: {
-        async searchQuery(searchQuery) {
-            if (!searchQuery) {
-                this.articles = [];
-                return;
-            }
-            this.articles = await this.$content("articles")
-                .limit(5)
-                .search(searchQuery)
-                .fetch();
-        }
-    }
+	data() {
+		return {
+			searchQuery: "",
+			articles: [],
+		};
+	},
+
+	watch: {
+		async searchQuery(searchQuery) {
+			if (!searchQuery) {
+				this.articles = [];
+				return;
+			}
+			this.articles = await this.$content("articles")
+				.limit(5)
+				.search(searchQuery)
+				.fetch();
+		},
+	},
 };
 </script>
--- components/TheNavbar.vue
+++ components/TheNavbar.vue
@@ -1,12 +1,12 @@
 <template>
     <nav class="py-4">
-        <ul class="md:flex justify-between px-8 md:px-16">
-            <li>
+        <ul class="flex justify-between items-center px-8 md:px-16">
+            <li class="w-1/2 md:w-2/3">
                 <nuxt-link to="/">
                     <img src="~/assets/logo.webp" class="logo" />
                 </nuxt-link>
             </li>
-            <li>
+            <li class="w-1/2 md:w-1/3">
                 <AppSearchInput />
             </li>
         </ul>
