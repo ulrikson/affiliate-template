7fa1dc514d75584f1b3eb8aaaedf364115615cc7
ulrikson <erik.ulrikson@gmail.com>
2021-07-27 14:32:18 +0200

    footer

--- components/TheFooter.vue
+++ components/TheFooter.vue
@@ -1,11 +1,6 @@
 <template>
-    <div class="bg-gray-bg bottom-0 mt-8 p-8 md:flex">
-        <div class="md:w-1/2 mr-4">
-            <h2>About Affiliate</h2>
-            <p class="mb-4">Animi asperiores ducimus in et ullam facilis nobis quibusdam. Nobis labore odio aut officiis adipisci.</p>
-        </div>
-        <div class="md:w-1/2">
-            <h2>Popular posts</h2>
-        </div>
-    </div>
+	<div class="flex justify-between mx-8 md:mx-16 mt-16 bottom-0 border-t-2 border-black border-solid">
+		<p class="my-4">EBU Group AB</p>
+		<p class="my-4"><a href="mailto:erik@ebugroup.se">erik@ebugroup.se</a></p>
+	</div>
 </template>
--- layouts/default.vue
+++ layouts/default.vue
@@ -2,6 +2,6 @@
     <div class="h-full">
         <the-navbar />
         <Nuxt class="px-8 md:px-16 mb-8" />
-        <!-- <the-footer /> -->
+        <the-footer />
     </div>
 </template>
