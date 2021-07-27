9b1fe3b430b9b3665b4a54bcba5ce2df5e874a72
ulrikson <erik.ulrikson@gmail.com>
2021-07-27 14:10:10 +0200

    font

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,5 +1,5 @@
 <template>
-    <div class="h-full font-sans">
+    <div class="h-full">
         <the-navbar />
         <Nuxt class="px-8 md:px-16 mb-8" />
         <!-- <the-footer /> -->
--- nuxt.config.js
+++ nuxt.config.js
@@ -1,50 +1,58 @@
 export default {
-    // Target: https://go.nuxtjs.dev/config-target
-    target: "static",
+	// Target: https://go.nuxtjs.dev/config-target
+	target: "static",
 
-    // Global page headers: https://go.nuxtjs.dev/config-head
-    head: {
-        title: "affiliate-template",
-        htmlAttrs: {
-        lang: "en"
-        },
-        meta: [
-        { charset: "utf-8" },
-        { name: "viewport", content: "width=device-width, initial-scale=1" },
-        { hid: "description", name: "description", content: "" },
-        { name: "format-detection", content: "telephone=no" }
-        ],
-        link: [{ rel: "icon", type: "image/x-icon", href: "/favicon.ico" }]
-    },
+	// Global page headers: https://go.nuxtjs.dev/config-head
+	head: {
+		title: "affiliate-template",
+		htmlAttrs: {
+			lang: "en",
+		},
+		meta: [
+			{ charset: "utf-8" },
+			{ name: "viewport", content: "width=device-width, initial-scale=1" },
+			{ hid: "description", name: "description", content: "" },
+			{ name: "format-detection", content: "telephone=no" },
+		],
+		link: [
+			{ rel: "icon", type: "image/x-icon", href: "/favicon.ico" },
+			{
+				rel: "stylesheet",
+				href: "https://fonts.googleapis.com/css2?family=Open+Sans&family=Poppins&display=swap",
+			},
+		],
+	},
 
-    // Global CSS: https://go.nuxtjs.dev/config-css
-    css: [
-        '~/assets/styles/index.css'
-    ],
+	// Global CSS: https://go.nuxtjs.dev/config-css
+	css: ["~/assets/styles/index.css"],
 
-    // Plugins to run before rendering page: https://go.nuxtjs.dev/config-plugins
-    plugins: [],
+	// Plugins to run before rendering page: https://go.nuxtjs.dev/config-plugins
+	plugins: [],
 
-    // Auto import components: https://go.nuxtjs.dev/config-components
-    components: true,
+	// Auto import components: https://go.nuxtjs.dev/config-components
+	components: true,
 
-    // Modules for dev and build (recommended): https://go.nuxtjs.dev/config-modules
-    buildModules: [
-        // https://go.nuxtjs.dev/tailwindcss
-        "@nuxtjs/tailwindcss",
-        ['nuxt-fontawesome', {
-            component: 'fa', //customize component name
-            imports: [{
-                set: '@fortawesome/free-solid-svg-icons',
-                    icons: ['faChevronRight', 'faLayerGroup', 'faPencilRuler', 'faLanguage', 'faBars']
-                }
-            ]
-        }]
-    ],
+	// Modules for dev and build (recommended): https://go.nuxtjs.dev/config-modules
+	buildModules: [
+		// https://go.nuxtjs.dev/tailwindcss
+		"@nuxtjs/tailwindcss",
+		[
+			"nuxt-fontawesome",
+			{
+				component: "fa", //customize component name
+				imports: [
+					{
+						set: "@fortawesome/free-solid-svg-icons",
+						icons: ["faChevronRight", "faLayerGroup", "faPencilRuler", "faLanguage", "faBars"],
+					},
+				],
+			},
+		],
+	],
 
-    // Modules: https://go.nuxtjs.dev/config-modules
-    modules: ['@nuxt/content'],
+	// Modules: https://go.nuxtjs.dev/config-modules
+	modules: ["@nuxt/content"],
 
-    // Build Configuration: https://go.nuxtjs.dev/config-build
-    build: {}
-    };
+	// Build Configuration: https://go.nuxtjs.dev/config-build
+	build: {},
+};
