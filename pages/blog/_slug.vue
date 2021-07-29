0b7518b2884189ecb74b6e970efe724710f2e75b
ulrikson <erik.ulrikson@gmail.com>
2021-07-29 15:24:33 +0200

    blog updates

--- pages/blog/_slug.vue
+++ pages/blog/_slug.vue
@@ -1,54 +1,47 @@
 <template>
-    <article>
-        <h1>{{ article.title }}</h1>
-        <div class="md:w-2/3 mx-auto">
-            <img
-                :src="require(`~/assets/images/${article.img}`)"
-                :alt="article.alt"
-                class="md:h-96 my-4 rounded-md object-cover w-full"
-            />
+	<article>
+		<h1>{{ article.title }}</h1>
+		<p class="text-center text-gray-500">Last updated: {{ formatDate(article.updatedAt) }}</p>
+		<div class="md:w-2/3 mx-auto">
+			<img
+				:src="require(`~/assets/images/${article.img}`)"
+				:alt="article.alt"
+				class="md:h-96 my-4 rounded-md object-cover w-full"
+			/>
+            <p>Photo credit: <a :href="article.credits" target="blank_" rel="nofollow">Unsplash</a></p>
 
-            <p>{{ article.description }}</p>
+			<p>{{ article.description }}</p>
 
-            <nav class="my-8">
-                <ul>
-                    <li v-for="link of article.toc" :key="link.id">
-                        <NuxtLink :to="`#${link.id}`">{{ link.text }}</NuxtLink>
-                    </li>
-                </ul>
-            </nav>
+			<nuxt-content :document="article" class="my-8" />
+		</div>
 
-            <nuxt-content :document="article" class="my-8" />
-        </div>
-
-        <prev-next :prev="prev" :next="next" />
-    </article>
+		<prev-next :prev="prev" :next="next" />
+	</article>
 </template>
 
 <script>
 export default {
-    async asyncData({ $content, params }) {
-        const article = await $content("articles", params.slug).fetch();
+	async asyncData({ $content, params }) {
+		const article = await $content('articles', params.slug).fetch();
 
-        return { article };
-    },
+		const [prev, next] = await $content('articles')
+			.only(['title', 'slug'])
+			.sortBy('createdAt', 'asc')
+			.surround(params.slug)
+			.fetch();
 
-    async asyncData({ $content, params }) {
-        const article = await $content("articles", params.slug).fetch();
+		return {
+			article,
+			prev,
+			next,
+		};
+	},
 
-        const [prev, next] = await $content("articles")
-            .only(["title", "slug"])
-            .sortBy("createdAt", "asc")
-            .surround(params.slug)
-            .fetch();
-
-        return {
-            article,
-            prev,
-            next
-        };
-    },
-
-    methods: {}
+	methods: {
+		formatDate(date) {
+			const options = { year: 'numeric', month: 'long', day: 'numeric' };
+			return new Date(date).toLocaleDateString('en', options);
+		},
+	},
 };
 </script>
