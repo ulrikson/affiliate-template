732c1413d1ac0363f10a3d9180f47af2c6ba93b0
ulrikson <erik.ulrikson@gmail.com>
2021-08-04 20:27:13 +0200

    font local

--- nuxt.config.js
+++ nuxt.config.js
@@ -16,10 +16,6 @@ export default {
 		],
 		link: [
 			{ rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' },
-			{
-				rel: 'stylesheet',
-				href: 'https://fonts.googleapis.com/css2?family=Poppins&display=swap',
-			},
 		],
 	},
 
