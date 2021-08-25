87b6261207511b9a51a95c0c5e1ccc6a7e8ac612
ulrikson <erik.ulrikson@gmail.com>
2021-08-25 20:54:04 +0200

    e-g temp

---
+++ pages/EpsilonGreedy.vue
@@ -0,0 +1,19 @@
+<template>
+	<div class="rounded-md py-4 px-8 bg-gray-800">
+		
+	</div>
+</template>
+
+<script>
+
+// MOVE TO GLOBAL COMPONENTS => into post
+
+export default {
+	data() {
+		return {
+
+		};
+	},
+
+};
+</script>
